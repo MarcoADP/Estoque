16ec4f4e44fb69b382815915a2a2d8c474081ab8
Marco Aurélio Paulino <marco23_aurelio@hotmail.com>
2016-02-13 15:56:30 -0200

    Caixa Branca - Marco - 100% Lembretes: Tirar blank das constraints porque os tests nao consegue simulá-los -- Caminho1 é para qnt negativa e nao para qtd venda maior que qtd estoque como diz no relatorio - lembrar de avisar isso ao professor na apresentacao

--- LojaDeCosmetico/test/unit/loja/TesteCaixaBrancaAdicionarProdutoSpec.groovy
+++ LojaDeCosmetico/test/unit/loja/TesteCaixaBrancaAdicionarProdutoSpec.groovy
@@ -1,6 +1,6 @@
 package loja
 
-import grails.test.GrailsUnitTestCase
+import grails.test.*
 
 class TesteCaixaBrancaAdicionarProdutoSpec extends GrailsUnitTestCase{
 
@@ -8,16 +8,39 @@ class TesteCaixaBrancaAdicionarProdutoSpec extends GrailsUnitTestCase{
 
     protected void tearDown() {super.tearDown()}
 
+    //Quantidade negativa
     void testeCaminho1(){
         mockDomain Venda
         mockDomain Produto
         mockDomain ItemVendaProduto
         Venda venda = new Venda(dataTransacao: new Date())
         Produto produto = new Produto(codigo : 10, nome: 'Esmalte Azul', preco : 2.99, quantidade: 2, dataFabricacao : new Date(2016, 01, 01), dataVencimento: new Date(2017, 01, 01))
-        int quantidade = 4
+        int quantidade = -1
+        assertFalse venda.addItemProduto(produto,quantidade)
+    }
+
+    //Produto com erro, ex: cod negativo
+    void testeCaminho2(){
+        mockDomain Venda
+        mockDomain Produto
+        mockDomain ItemVendaProduto
+        Venda venda = new Venda(dataTransacao: new Date())
+        Produto produto = new Produto(codigo : -10, nome: 'Esmalte Azul', preco : 2.99, quantidade: 2, dataFabricacao : new Date(2016, 01, 01), dataVencimento: new Date(2017, 01, 01))
+        int quantidade = -1
+
+        assertFalse venda.addItemProduto(produto,quantidade)
+    }
+
+   //Tudo OK
+    void testeCaminho3(){
+        mockDomain Venda
+        mockDomain Produto
+        mockDomain ItemVendaProduto
+        Venda venda = new Venda(dataTransacao: new Date())
+        Produto produto = new Produto(codigo : 10, nome: 'Esmalte Azul', preco : 2.99, quantidade: 2, dataFabricacao : new Date(2016, 01, 01), dataVencimento: new Date(2017, 01, 01))
+        int quantidade = 1
 
         assertTrue venda.addItemProduto(produto,quantidade)
-
-
     }
+
 }
